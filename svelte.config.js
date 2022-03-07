fe760a3c20715c11983e955b9088292653f1d752
Ivan Hofer <ivan.hofer@outlook.com>
2022-03-07 17:37:00 +0100

    add stricter typescript checks

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto'
+import preprocess from 'svelte-preprocess'
+import { resolve } from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,8 +9,16 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
-	}
-};
+		adapter: adapter(),
+		vite: {
+			resolve: {
+				alias: {
+					$lib: resolve('./src/lib'),
+					$models: resolve('./src/models'),
+				},
+			},
+		},
+	},
+}
 
-export default config;
+export default config
