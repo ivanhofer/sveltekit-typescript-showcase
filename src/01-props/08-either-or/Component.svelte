c824e45338ffc1a9c907c63d00a6a0af4884a0e9
Ivan Hofer <ivan.hofer@outlook.com>
2022-04-22 16:10:32 +0200

    add comments to `either or` example

--- src/01-props/08-either-or/Component.svelte
+++ src/01-props/08-either-or/Component.svelte
@@ -1,22 +1,32 @@
 <script lang="ts">
+	// we need to use two generic types
+	// on the first we say it is either from some type (`string`) or `undefined`
 	type Title = $$Generic<string | undefined>
-	type Label = $$Generic<Title extends string ? undefined : string>
+	// for the second generic type we use a conditional type and assign it
+	// either to some type (`string`) if the other type is `undefined`
+	// or to `undefined` if the other type is defined
+	type Label = $$Generic<Title extends undefined ? string : undefined>
 
+	// define the props this component always has
 	interface Props {
 		name: string
 	}
 
+	// define the props this component can have in variant A
 	interface WithLabel extends Props {
 		label: Label
 	}
 
+	// define the props this component can have in variant B
 	interface WithTitle extends Props {
 		title: Title
 	}
 
+	// combine both variants via an union type
 	type $$Props = WithLabel | WithTitle
 
 	export let name: string
+	// we need to cast `undefined` to the variable we haave specified
 	export let label: Label = undefined as Label
 	export let title: Title = undefined as Title
 </script>
