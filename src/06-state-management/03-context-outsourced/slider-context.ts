a4ff1d9e430afd1d2e7e8881fa24a05b4d0062aa
Ivan Hofer <ivan.hofer@outlook.com>
2022-06-09 21:28:43 +0200

    throw an Error if context was not set before

--- src/06-state-management/03-context-outsourced/slider-context.ts
+++ src/06-state-management/03-context-outsourced/slider-context.ts
@@ -27,4 +27,14 @@ export const setSliderContext = (items: SliderItem[]) =>
 	})
 
 // our getter simply wraps the `getContext` function and its type declaration
-export const getSliderContext = () => getContext<SliderContext>(KEY)
+export const getSliderContext = () => {
+	const context = getContext<SliderContext>(KEY)
+	if (context !== undefined)
+		return context
+
+	// TypeScript thinks the function will always return a value of type `SliderContext`,
+	// but it's not true in the case we forgot to call `setSliderContext` first.
+	// In this case it would return `undefined`, which could lead to an unexpected behavior,
+	// so instead we'll throw an error which will inform you about the issue.
+	throw new Error('you forgot to set the Slider context before accessing it')
+}
